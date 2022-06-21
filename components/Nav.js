39feb67b375567a15306fc10c21969a94d6099e4
Lucas Santos <leos9877@gmail.com>
2022-06-21 00:17:28 -0300

    :rocket: create new NavBar.

---
+++ components/Nav.js
@@ -0,0 +1,17 @@
+import navStyles from '../styles/Nav.module.css';
+import Link from 'next/link';
+
+export const Nav = () => {
+    return (
+        <nav className={navStyles.nav}>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
