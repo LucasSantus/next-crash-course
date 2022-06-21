466292b8d2eb3e847bd15a92aba7fabef50c7e4b
Lucas Santos <leos9877@gmail.com>
2022-06-21 00:01:08 -0300

    :rocket: create layout for project

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+import styles from './styles/Layout.module.css';
+
+export const Layout = ({children}) => {
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                {children}
+            </main>
+        </div>
+    )
+}
