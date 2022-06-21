2901208e466bdeb1bd9cd0fae1b5d94942506432
Lucas Santos <leos9877@gmail.com>
2022-06-20 23:55:04 -0300

    :pushpin: add Head in index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,14 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
-      <h1>Welcome </h1>
+      <Head>
+        <title>NextJS Crash Course</title>
+        <meta name="keywords" content="nextjs crash course" />
+        <meta name="description" content="NextJS Crash Course " />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-scale=1" />
+      </Head>
+
+      <h1>Welcome to NextJS</h1>
     </div>
   )
 }
