221268faf6b6e9a0503693df2c8b04a2dc151d3a
Lucas Santos <leos9877@gmail.com>
2022-06-21 23:11:02 -0300

    :pushpin: add new funtion getStaticProps.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ articles}) {
+  console.log(articles)
   return (
     <div>
       <Head>
@@ -13,4 +14,15 @@ export default function Home() {
       <h1>Welcome to NextJS</h1>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
